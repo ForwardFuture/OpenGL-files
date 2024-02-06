b213f28f016631208b414bdab78ed78cf39500bf
ForwardFuture <48513897+ForwardFuture@users.noreply.github.com>
2024-02-06 16:09:39 +0800

    Add files via upload

---
+++ Global.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <cstdio>
+#include <iostream>
+
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
+#include "stb_image.h"
+
+using namespace std;
